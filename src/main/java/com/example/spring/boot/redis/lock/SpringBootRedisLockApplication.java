78c92c60dfb949848aa24a48a182e3ecd7a1cb93
Jiwoong Choi <sea4choi@gmail.com>
2023-09-23 22:09:13 +0900

    first commit

---
+++ src/main/java/com/example/spring/boot/redis/lock/SpringBootRedisLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.redis.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisLockApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRedisLockApplication.class, args);
+  }
+
+}
